34de01ef4f394c84129218df9e1ad874661f29db
Abdallah Waleed Kamal <waleedabdallah238@gmail.com>
2026-09-15 13:51:21 +0300

    Add files via upload

--- DTOs/Common/PagedResult.cs
+++ DTOs/Common/PagedResult.cs
@@ -7,6 +7,6 @@ public class PagedResult<T>
     public int PageSize { get; set; }
     public int TotalCount { get; set; }
     public int TotalPages => (int)Math.Ceiling((double)TotalCount / PageSize);
-    public bool HasNextPage => Page < TotalPages;
     public bool HasPreviousPage => Page > 1;
+    public bool HasNextPage => Page < TotalPages;
 }
